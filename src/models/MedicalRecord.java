a44a90274ec8284ba5d2f286d8b7dc54915325d3
Yousef Mohamed <yousefelsise440@gmail.com>
2026-03-15 00:31:26 +0200

    Add MedicalRecod Class

--- src/models/MedicalRecord.java
+++ src/models/MedicalRecord.java
@@ -1,34 +1,20 @@
-package hospital;
-interface Printable {
-    void printInfo();
-}
-class Document {
-protected String recordID;
-public Document(String recordID) {
-    this.recordID = recordID;
-    }
+package models;
 
-    
-}
-public class MedicalRecord extends Document implements Printable {
-private String diagnosis;
+public class MedicalRecord implements Printable {
+    private String recordID;
+    private String diagnosis;
     private String prescription;
     private String date;
     private String doctorName;
+
     public MedicalRecord(String recordID, String diagnosis, String prescription, String date, String doctorName) {
-        super (recordID);
+        this.recordID = recordID;
         this.diagnosis = diagnosis;
         this.prescription = prescription;
         this.date = date;
         this.doctorName = doctorName;
     }
-     super(recordID); 
-     this.diagnosis = diagnosis;
-     this.prescription = prescription;
-        this.date = date;
-        this.doctorName = doctorName;
 
-   @Override
     public void printInfo() {
         System.out.println("Record ID: " + recordID);
         System.out.println("Diagnosis: " + diagnosis);
@@ -36,16 +22,25 @@ private String diagnosis;
         System.out.println("Date: " + date);
         System.out.println("Doctor Name: " + doctorName);
     }
+
     public String getRecordID() {
         return recordID;
-    }public String getDiagnosis() {
-        return diagnosis;
-    }public String getPrescription() {
-        return prescription;
-    }public String getDate() {
-        return date;
-    }public String getDoctorName() {
-        return doctorName;
     }
 
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public String getPrescription() {
+        return prescription;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getDoctorName() {
+        return doctorName;
+
+    }
 }
