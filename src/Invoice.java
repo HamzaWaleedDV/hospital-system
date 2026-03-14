70b9fd182e5f86c667693128a734745ac813e368
Adham El Azab <adhammo7amed2025@gmail.com>
2026-03-14 13:49:57 +0200

    Add Invoice project file (one_class)

---
+++ src/Invoice.java
@@ -0,0 +1,53 @@
+public class Invoice {
+    private String invoiced;
+    private Patient patient;
+    private double consultationFee;
+    private double roomCharges;
+    private double medicationCharges;
+    private boolean isPaid;
+    private double totalPaid;
+
+    Invoice(String invoiced, Patient patient, double consultationFee, double roomCharges, double medicationCharges) {
+        this.invoiced = invoiced;
+        this.patient = patient;
+        this.consultationFee = consultationFee;
+        this.roomCharges = roomCharges;
+        this.medicationCharges = medicationCharges;
+        this.isPaid = false;
+    }
+
+    public void calculateTotal() {
+         totalPaid = consultationFee + roomCharges + medicationCharges;
+    }
+
+    public double getTotal() {
+        return totalPaid ;
+    }
+
+    public void pay() {
+        isPaid = true;
+    }
+
+    public boolean isPaid() {
+        return isPaid;
+    }
+
+    public String getInvoicedId() {
+        return patient.getId().toString();
+    }
+    public void printinfo() {
+        System.out.println("Invoice for: " + patient.getName() + " (ID: " + patient.getId() + ")\n");
+
+        System.out.println("Consultation Fee: $" + consultationFee + "\n");
+
+        System.out.println("Room Charges: $" + roomCharges + "\n");
+
+        System.out.println("Medication Charges: $" + medicationCharges + "\n");
+
+        System.out.println("Total Amount: $" + getTotal() + "\n");
+
+        System.out.println("Payment Status: " + (isPaid ? "Paid" : "Unpaid") + "\n");
+    }
+
+
+}
