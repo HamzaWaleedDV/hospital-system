ee4bd3c44721cdf8d7d8cf323c42a662529bcdf2
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-03-31 21:52:31 +0200

    .

--- src/models/Patient.java
+++ src/models/Patient.java
@@ -11,38 +11,36 @@ public class Patient extends Person{
     private List<MedicalRecord> medicalHistory;
 
     public Patient (String id , String name , int age , Gender gender , String phone , BloodType bloodType ){
-
       super( id , name , age , gender , phone);
       this.bloodType = bloodType;
       this.medicalHistory = new ArrayList<>();
     }
 
-     @Override
-    public String getRole(){
-         return "patient";
+    @Override
+    public String getRole() {
+        return "patient";
 
     }
-       @Override
-    public void printInfo(){
 
+    @Override
+    public void printInfo() {
         super.printInfo();
         System.out.println("Blood type : " + bloodType);
         printMedicalHistory();
     }
-    public void addMedicalRecord(MedicalRecord record){
 
+    public void addMedicalRecord(MedicalRecord record) {
         medicalHistory.add(record);
     }
 
-  public void printMedicalHistory() {
+    public void printMedicalHistory() {
         System.out.println("Medical History:");
         for (MedicalRecord record : medicalHistory) {
             System.out.println(record);
-        }}
+        }
+    }
     
-
     public BloodType getBloodType(){
-
         return bloodType;
     }
 
